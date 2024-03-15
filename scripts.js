ec54dda19f3dd79f34ffb68f0081a194d1ecfed5
joaogabriell10 <jg521691@gmail.com>
2024-03-15 07:38:34 -0300

    Var nome

--- scripts.js
+++ scripts.js
@@ -4,6 +4,6 @@ var nome = document.getElementById("nome");
 
 function alertar (event){
     if(true){
-        alert("Você clicou no botão!!!" + nome.value);
+        alert("Você clicou no botão!!!" + " " + nome.value);
     }
 }
